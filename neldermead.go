d1109c9caa1c2dac949409f7ba2f48810ea695b5
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2013-12-13 17:41:21 +0100

    Vastly improve evaluation count and iteration time on the data.

--- neldermead.go
+++ neldermead.go
@@ -5,9 +5,9 @@ import (
 )
 
 const (
-	kMax = 1000             // arbitrarily chosen value for now
-	ε    = 0.00000000000001 // Stopping criterion point
-	α    = 1.2
+	kMax = 200          // arbitrarily chosen value for now
+	ε    = 0.0000000001 // Stopping criterion point
+	α    = 1.0
 	β    = 0.5
 	γ    = 2.0
 )
@@ -38,6 +38,8 @@ func Optimize(f optfunc, start [][]float64) ([]float64, int, int) {
 	n := len(start)
 	c := len(start[0])
 	points := make([]point, 0)
+	fv := make([]float64, n)
+
 	for _, p := range start {
 		points = append(points, point(p))
 	}
@@ -46,43 +48,119 @@ func Optimize(f optfunc, start [][]float64) ([]float64, int, int) {
 		panic("Can't optimize with too few starting points")
 	}
 
-	var l int
+	// Set up initial values
+	for i := range fv {
+		fv[i] = eval(f, sx[i])
+	}
+
 	k := 0
-	for ; stopCriterion(f, sx) && k < kMax; k++ {
-		h := sx.argMax(f)
-		l = sx.argMin(f)
+	for ; k < kMax; k++ {
+		// Find the largest index
+		vg := 0
+		for i := range fv {
+			if fv[i] > fv[vg] {
+				vg = i
+			}
+		}
 
-		xp := sub(
-			sx.centroid(h).scale(1.0+α),
-			sx[h].scale(α))
+		// Find the smallest index
+		vs := 0
+		for i := range fv {
+			if fv[i] < fv[vs] {
+				vs = i
+			}
+		}
 
-		if eval(f, xp) < eval(f, sx[l]) {
-			xpp := sub(xp.scale(1.0-γ), sx.centroid(h).scale(γ))
-			if eval(f, xpp) < eval(f, sx[l]) {
-				sx[h] = xpp // Expansion
+		// Second largest index
+		vh := vs
+		for i := range fv {
+			if fv[i] > fv[vh] && fv[i] < fv[vg] {
+				vh = i
+			}
+		}
+
+		vm := sx.centroid(vg)
+
+		vr := add(vm, sub(vm, sx[vg]).scale(α))
+
+		fr := eval(f, vr)
+
+		if fr < fv[vh] && fr >= fv[vs] {
+			// Replace
+			fv[vg] = fr
+			sx[vg] = vr
+		}
+
+		// Investigate a step further
+		if fr < fv[vs] {
+			ve := add(vm, sub(vr, vm).scale(γ))
+
+			fe := eval(f, ve)
+
+			if fe < fr {
+				sx[vg] = ve
+				fv[vg] = fe
 			} else {
-				sx[h] = xp // Reflection
+				sx[vg] = vr
+				fv[vg] = fr
 			}
-		} else if testForallBut(f, xp, sx, h) {
-			if eval(f, xp) <= eval(f, sx[h]) {
-				sx[h] = xp
+		}
+
+		// Check contraction
+		if fr >= fv[vh] {
+			var vc point
+			var fc float64
+			if fr < fv[vg] && fr >= fv[vh] {
+				// Outside contraction
+				vc = add(vm, sub(vr, vm).scale(β))
+
+				fc = eval(f, vc)
+			} else {
+				// Inside contraction
+				vc = sub(vm, sub(vm, sx[vg]).scale(β))
+				fc = eval(f, vc)
 			}
 
-			xpp := add(sx[h].scale(β), sx.centroid(h).scale(1.0-β))
-			if eval(f, xpp) > eval(f, sx[h]) {
-				// Multiple contraction
+			if fc < fv[vg] {
+				sx[vg] = vc
+				fv[vg] = fc
+			} else {
 				for i := range sx {
-					sx[i] = add(sx[i], sx[l]).scale(0.5)
+					if i != vs {
+						sx[i] = add(sx[vs], sub(sx[i], sx[vs]).scale(0.5))
+					}
 				}
-			} else {
-				sx[h] = xpp // Contraction
+
+				fv[vg] = eval(f, sx[vg])
+				fv[vh] = eval(f, sx[vh])
 			}
-		} else {
-			sx[h] = xp // Reflection
+		}
+
+		fsum := 0.0
+		for _, v := range fv {
+			fsum += v
+		}
+
+		favg := fsum / float64(len(fv)+1)
+		s := 0.0
+		for _, v := range fv {
+			s += math.Pow(v-favg, 2.0) / float64(n)
+		}
+
+		s = math.Sqrt(s)
+		if s < ε {
+			break
 		}
 	}
 
-	return sx[l], k, evaluations
+	vs := 0
+	for i := range fv {
+		if fv[i] < fv[vs] {
+			vs = i
+		}
+	}
+
+	return sx[vs], k, evaluations
 }
 
 // sub perform point subtraction
@@ -118,23 +196,6 @@ func (p point) scale(scalar float64) point {
 	return r
 }
 
-// testForallBut() is a helper function for the optimization to simplify a predicate
-func testForallBut(f optfunc, xp point, sx simplex, h int) bool {
-	for i := range sx {
-		if i == h {
-			continue
-		} else {
-			if eval(f, xp) > eval(f, sx[i]) {
-				continue
-			} else {
-				return false
-			}
-		}
-	}
-
-	return true
-}
-
 // centroid calculates the centroid of a simplex of one dimensionality lower by omitting a point
 func (s simplex) centroid(omit int) point {
 	r := make(point, len(s[0]))
@@ -153,57 +214,4 @@ func (s simplex) centroid(omit int) point {
 	}
 
 	return r
-}
-
-// argMax finds the best point in the simplex for the optfunc
-func (s simplex) argMax(f optfunc) (idx int) {
-	v := eval(f, s[0])
-	idx = 0
-	for i := 1; i < len(s); i++ {
-		r := eval(f, s[i])
-		if r > v {
-			v = r
-			idx = i
-		}
-	}
-	return
-}
-
-// argMin finds the worst point in the simplex for the optfunc
-func (s simplex) argMin(f optfunc) (idx int) {
-	v := eval(f, s[0])
-	idx = 0
-	for i := 1; i < len(s); i++ {
-		r := eval(f, s[i])
-		if r < v {
-			v = r
-			idx = i
-		}
-	}
-	return
-}
-
-// stopCriterion tests if the stop criterion has been met
-func stopCriterion(f optfunc, xs simplex) bool {
-	r := make([]float64, len(xs))
-
-	for i := range r {
-		r[i] = f(xs[i])
-	}
-
-	sum := 0.0
-	for _, v := range r {
-		sum += v
-	}
-	avg := sum / float64(len(r))
-
-	s := 0.0
-	for _, v := range r {
-		rs := (v - avg) * (v - avg)
-		s += rs
-	}
-
-	res := math.Sqrt(1 / (1.0 + float64(len(r))) * s)
-
-	return (res > ε)
 }
